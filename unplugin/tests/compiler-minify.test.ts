22bebea32ab39b186583c73027a0389566b01a19
tingyuan <hbtwhucsis@gmail.com>
2023-12-22 22:44:37 +0800

    0.0.1

--- unplugin/tests/compiler-minify.test.ts
+++ unplugin/tests/compiler-minify.test.ts
@@ -5,7 +5,7 @@ import { compileTemplate } from '../src/compiler'
 const file1 = `
 <p a>sdkfjdsl</p>
 
-<div fd="sdf  "   ff="">a   </div>
+<div fd="sdf  " >a</div>
 `
 
 describe('htm-compiler', () => {
@@ -13,6 +13,6 @@ describe('htm-compiler', () => {
     const context = {}
     const { code, template } = await compileTemplate.call(context, file1, 'a-b', false)
     assert.ok(code === '')
-    assert.equal(template, '<p a>sdkfjdsl</p><div fd="sdf  " ff="">a   </div>')
+    assert.equal(template, '<p a>sdkfjdsl</p><div fd="sdf  " >a</div>')
   })
 })
