c84792a6403180d9b40ef363940fa89992a002c5
tingyuan <1932294867@qq.com>
2024-01-02 18:45:42 +0800

    -

--- playground/bare/component.js
+++
@@ -1,6 +0,0 @@
-import { parseHTML } from './compiler.js'
-
-const _module = import.meta.url.split('?').pop()
-const url = import.meta.resolve(`./components/${_module}.htm`)
-
-export default await parseHTML(url, _module)
